93274da81ee47ca17a7cedb0dbf153e33b3fe1f9
qhenkart <qhenkart@gmail.com>
2015-05-22 15:16:42 -0700

    cleaned up some extra code

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,44 @@
 var express = require('express');
 
+
 // module.exports = router;
+
+
+
 
 module.exports = function(passport) {
     var router = express.Router();
     router.get('/', ensureAuthenticated, function(req, res){
-      res.render('index', { user: req.user });
+      res.render('index', { title: 'express' });
     });
 
-    router.get('/auth/facebook', passport.authenticate('facebook'), function(req, res){
-    // The request will be redirected to Facebook for authentication, so this
-    // function will not be called.
-    });
 
+/// GET /auth/facebook
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Facebook authentication will involve
+//   redirecting the user to facebook.com.  After authorization, Facebook will
+//   redirect the user back to this application at /auth/facebook/callback
+  router.get('/auth/facebook', passport.authenticate('facebook'), function(req, res){
+  // The request will be redirected to Facebook for authentication, so this
+  // function will not be called.
+  });
+
+
+
+// GET /auth/facebook/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
   router.get('/auth/facebook/callback', 
     passport.authenticate('facebook', { failureRedirect: '/login' }),
     function(req, res) {
-      debugger;
       res.redirect('/');
+  });
+
+  router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
   });
     // etc.
 
