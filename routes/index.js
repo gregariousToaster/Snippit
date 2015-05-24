0344e287d154b3705d5342e01e231cdb25fc80e6
qhenkart <qhenkart@gmail.com>
2015-05-23 18:40:08 -0700

    fixed merge request

--- routes/index.js
+++ routes/index.js
@@ -39,7 +39,6 @@ module.exports = function(passport) {
   router.get('/auth/facebook/callback', 
     passport.authenticate('facebook', { failureRedirect: '/login' }),
     function(req, res) {
-      debugger;
       utils.checkData(req, res, function(){
         res.redirect('/');  
       })
