fa77452b187f2ba6bc9d2089a9ef39b7b20dce8a
qhenkart <qhenkart@gmail.com>
2015-05-23 18:38:56 -0700

    added mongoDB database to track users with facebook oAuth

--- server/utils.js
+++ server/utils.js
@@ -6,7 +6,6 @@ exports.handleFacebookData = function(){
     if (err) {
       return console.log(err, "error on intialize");
     }else{
-      debugger;
       var dat = JSON.parse(data);
       
     }
