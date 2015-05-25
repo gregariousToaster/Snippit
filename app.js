b1a99e613d3be237caa5844a72d9241074567302
Khoa Phan <khoaphan89@gmail.com>
2015-05-25 12:07:08 -0700

    Updates and clean-up. See notes:
    - Handle all 'views' with AngularJS states
    - Delete 'views' directory (use templates dir from now on)
    - Change .bowerrc path to public/ from app/ (no longer exists)

--- app.js
+++ app.js
@@ -23,13 +23,6 @@ var app = express();
 
 require('./server/config/passport.js')(passport); // pass passport for configuration
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
