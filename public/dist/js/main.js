38bcf7af67d1f3a8319702bdfc875bf0edc94f55
Khoa Phan <khoaphan89@gmail.com>
2015-05-28 19:30:06 -0700

    Remove Material Design, add autocomplete, AngularJS changes

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -5,6 +5,7 @@ angular.module('snippit', ['snippit.main',
   'snippit.three',
   'snippit.auth',
   'snippit.search',
+  'autocomplete',
   'ui.router'
   ])
   .run(['$rootScope', '$location', '$http', function($rootScope, $location, $http) {
@@ -143,119 +144,22 @@ angular.module('snippit.auth', ['snippit'])
 
 'use strict';
 
+angular.module('snippit.search', ['snippit'])
+  .controller('SearchController', ['$scope', function($scope) {
+    $scope.albums = ['khoa', 'jeff', 'john', 'victoria']
+
+    $scope.query = "";
+
+    $scope.returnThis = function() {
+      console.log('querrrryyyy', $scope.query);
+    }
+}])
+
+'use strict';
+
 angular.module('snippit.main', ['snippit', 'snippit.services'])
   .controller('MainController', ['Facebook', '$scope', function(Facebook, $scope) {
   }]);
-
-'use strict';
-
-angular.module('snippit.services', ['snippit'])
-  .factory('ThreeFactory', function() {
-
-
-    var createScene = function(i, collection, scene, objects, click){
-      var el = document.createElement('div');
-      el.className = 'element';
-
-      var image = document.createElement('img');
-      image.src = collection[i].source;
-      el.appendChild(image);
-
-      var object = new THREE.CSS3DObject(el);
-      object.position.x = Math.random() * 4000 - 2000;
-      object.position.y = Math.random() * 4000 - 2000;
-      object.position.z = Math.random() * 4000 - 2000;
-      scene.add(object);
-
-      if(click){
-        var bound = click.bind(i);
-        el.addEventListener('click', bound);
-      }
-
-      objects.push(object);
-    };
-
-    var table = function(n, i, target){
-      var object = new THREE.Object3D();
-      object.position.x = ((i % n) * 140) - 280;
-      object.position.y = -((Math.floor(i / n) + 1) * 180) + 540;
-      target.push(object);
-    };
-
-
-    var sphere = function(i, vector, target, r, len){
-      var phi = Math.acos(-1 + (2 * i) / len);
-      var theta = Math.sqrt(len * Math.PI) * phi;
-
-      var object = new THREE.Object3D();
-
-      object.position.x = r * Math.cos(theta) * Math.sin(phi);
-      object.position.y = r * Math.sin(theta) * Math.sin(phi);
-      object.position.z = r * Math.cos(phi);
-
-      vector.copy(object.position).multiplyScalar(2);
-
-      object.lookAt(vector);
-
-      target.push(object);
-    };
-
-
-    var helix = function(n, i, vector, target, spacing, offset, xRad, zRad, step){
-      var object = new THREE.Object3D();
-      var phi = i * spacing + (i % n)/n * (Math.PI * 2);
-
-      object.position.x = xRad * Math.sin(phi);
-      object.position.y = -(i * step) + offset;
-      object.position.z = zRad * Math.cos(phi);
-
-      vector.x = object.position.x * 2;
-      vector.y = object.position.y;
-      vector.z = object.position.z * 2;
-
-      object.lookAt(vector);
-      target.push(object);
-    };
-
-    var grid = function(n, i, target){
-      var object = new THREE.Object3D();
-
-      object.position.x = ((i % n) * 400) - 800;
-      object.position.y = (-(Math.floor(i / n) % n) * 400) + 800;
-      object.position.z = (Math.floor(i / (n * n))) * 1000 - 2000;
-
-      target.push(object);
-    };
-
-    return {
-      createScene: createScene,
-      table: table,
-      sphere: sphere,
-      helix: helix,
-      grid: grid
-    };
-  })
-  .factory('Facebook', ['$http', function($http) {
-
-    var getWallData = function() {
-      return $http.get('/getFacebookWall');
-    };
-
-    var getAlbumData = function() {
-      return $http.get('/getFacebookAlbums');
-    }
-
-    return {
-      getWallData: getWallData,
-      getAlbumData: getAlbumData
-    };
-  }])
-;
-
-'use strict';
-
-angular.module('snippit.search', ['snippit'])
-  .controller('SearchController', function() {})
 
 'use strict';
 
@@ -378,3 +282,108 @@ angular.module('snippit.three', ['snippit'])
     };
   }]);
 
+
+'use strict';
+
+angular.module('snippit.services', ['snippit'])
+  .factory('ThreeFactory', function() {
+
+
+    var createScene = function(i, collection, scene, objects, click){
+      var el = document.createElement('div');
+      el.className = 'element';
+
+      var image = document.createElement('img');
+      image.src = collection[i].source;
+      el.appendChild(image);
+
+      var object = new THREE.CSS3DObject(el);
+      object.position.x = Math.random() * 4000 - 2000;
+      object.position.y = Math.random() * 4000 - 2000;
+      object.position.z = Math.random() * 4000 - 2000;
+      scene.add(object);
+
+      if(click){
+        var bound = click.bind(i);
+        el.addEventListener('click', bound);
+      }
+
+      objects.push(object);
+    };
+
+    var table = function(n, i, target){
+      var object = new THREE.Object3D();
+      object.position.x = ((i % n) * 140) - 280;
+      object.position.y = -((Math.floor(i / n) + 1) * 180) + 540;
+      target.push(object);
+    };
+
+
+    var sphere = function(i, vector, target, r, len){
+      var phi = Math.acos(-1 + (2 * i) / len);
+      var theta = Math.sqrt(len * Math.PI) * phi;
+
+      var object = new THREE.Object3D();
+
+      object.position.x = r * Math.cos(theta) * Math.sin(phi);
+      object.position.y = r * Math.sin(theta) * Math.sin(phi);
+      object.position.z = r * Math.cos(phi);
+
+      vector.copy(object.position).multiplyScalar(2);
+
+      object.lookAt(vector);
+
+      target.push(object);
+    };
+
+
+    var helix = function(n, i, vector, target, spacing, offset, xRad, zRad, step){
+      var object = new THREE.Object3D();
+      var phi = i * spacing + (i % n)/n * (Math.PI * 2);
+
+      object.position.x = xRad * Math.sin(phi);
+      object.position.y = -(i * step) + offset;
+      object.position.z = zRad * Math.cos(phi);
+
+      vector.x = object.position.x * 2;
+      vector.y = object.position.y;
+      vector.z = object.position.z * 2;
+
+      object.lookAt(vector);
+      target.push(object);
+    };
+
+    var grid = function(n, i, target){
+      var object = new THREE.Object3D();
+
+      object.position.x = ((i % n) * 400) - 800;
+      object.position.y = (-(Math.floor(i / n) % n) * 400) + 800;
+      object.position.z = (Math.floor(i / (n * n))) * 1000 - 2000;
+
+      target.push(object);
+    };
+
+    return {
+      createScene: createScene,
+      table: table,
+      sphere: sphere,
+      helix: helix,
+      grid: grid
+    };
+  })
+  .factory('Facebook', ['$http', function($http) {
+
+    var getWallData = function() {
+      return $http.get('/getFacebookWall');
+    };
+
+    var getAlbumData = function() {
+      return $http.get('/getFacebookAlbums');
+    }
+
+    return {
+      getWallData: getWallData,
+      getAlbumData: getAlbumData
+    };
+  }])
+;
--- public/js/search/searchController.js
+++ public/js/search/searchController.js
@@ -1,4 +1,12 @@
 'use strict';
 
 angular.module('snippit.search', ['snippit'])
-  .controller('SearchController', function() {})
+  .controller('SearchController', ['$scope', function($scope) {
+    $scope.albums = ['person1', 'person2', 'person3', 'person4', 'person5']
+
+    $scope.query = "";
+
+    $scope.returnThis = function() {
+      console.log('querrrryyyy', $scope.query);
+    }
+}])
