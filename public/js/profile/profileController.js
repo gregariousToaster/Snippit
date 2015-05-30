b1b857553f74d4b0c8b84e73334945b1536dbd1b
Khoa Phan <khoaphan89@gmail.com>
2015-05-29 17:05:50 -0700

    Add documentation for profileController.js
    - Add comments detailing functions and variables
    - Re-name functions and variables to clearly reflect use

--- public/js/profile/profileController.js
+++ public/js/profile/profileController.js
@@ -3,26 +3,42 @@
 angular.module('snippit.profile', ['snippit'])
   .controller('ProfileController', ['$scope', 'Facebook', function($scope, Facebook) {
 
-    $scope.snippits = [];
+    // Album names
+    $scope.albumNames = [];
+
+    // Album photos
     $scope.albumPhotos = [];
+
+    // Parsed data
     $scope.parse = null;
 
-    $scope.snipClick = function(name, id) {
+    /* This function is invoked every time an album name is clicked on the
+     * profile page. It passes the Facebook getAlbumPhotos method the name
+     * and ID of the clicked album, which returns a promise. Upon success,
+     * we are given a response, which are the photos for that specific Facebook
+     * album. We then parse the data and push it to $scope.albumPhotos.
+     */
+    $scope.albumClick = function(name, id) {
       $scope.albumPhotos = [];
       Facebook.getAlbumPhotos(name, id).success(function(resp) {
         $scope.parse = JSON.parse(resp);
         for (var key in $scope.parse) {
           $scope.albumPhotos.push($scope.parse[key]);
         }
-        console.log('$SCOPE ALBUM PHOTOS', $scope.albumPhotos);
+        console.log('$scope.albumPhotos: ', $scope.albumPhotos);
       });
     };
+
+    /* This function is invoked on initialize of this controller. It fetches
+     * the album names for the logged in Facebook user, which allows them to
+     * select an album to fetch photos from.
+     */
 
     $scope.init = function() {
       Facebook.getAlbumData().success(function(resp) {
         $scope.parse = JSON.parse(resp);
         for (var key in $scope.parse) {
-          $scope.snippits.push($scope.parse[key]);
+          $scope.albumNames.push($scope.parse[key]);
         }
       });
     };
