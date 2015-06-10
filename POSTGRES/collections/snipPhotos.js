2f996ddcf053b509b471718f77dbac2005ac4e57
EOutterson <eoutterson@gmail.com>
2015-06-09 19:43:43 -0700

    cleared out example file

--- POSTGRES/collections/snipPhotos.js
+++
@@ -1,8 +0,0 @@
-var db = require('../config');
-var SnipPhotos = require('../models/snipPhoto');
-
-var SnipPhotos = new db.Collection();
-
-SnipPhotos.model = SnipPhoto;
-
-module.exports = SnipPhotos;
--- POSTGRES/collections/wallPhotos.js
+++
@@ -1,8 +0,0 @@
-var db = require('../config');
-var WallPhoto = require('../models/wallPhoto');
-
-var WallPhotos = new db.Collection();
-
-WallPhotos.model = WallPhoto;
-
-module.exports = WallPhotos;
