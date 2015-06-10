7916528c688347e2c9fa85fa42bf8aa401ad369d
Khoa Phan <khoaphan89@gmail.com>
2015-06-10 10:51:33 -0700

    Migrate from MongoDB to Postgres for Passport.js
    - Adds a user to the database with facebookID, name, and token
    - Updates user token on login
    - Deserializes user

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,25 +8,25 @@ var User = require('../../app/models/user');
 module.exports = function(passport) {
 // Passport session setup.
 //   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session. 
+//   serialize users into and deserialize users out of the session.
   passport.serializeUser(function(user, done) {
     done(null, user.id);
   });
 
 
   passport.deserializeUser(function(id, done) {
-    client.then(function(db){
-      return db.collection('users').findOneAsync({id: id})
-        .then(function(user){
-          if(!user) {
-            console.log("user not found in desiralize")
+        new User({facebookID: profile.id})
+        .fetch()
+        .then(function(model) {
+          if (!model) {
+            console.log('user not found for deserialize')
           }
-          done(null, user);
-          
-        });
-    })
+          else {
+            done(null, model);
+          }
+        })
   });
-   
+
 
 
 // Use the FacebookStrategy within Passport.
@@ -41,54 +41,27 @@ module.exports = function(passport) {
     function(accessToken, refreshToken, profile, done) {
 //saves user data into database or logs them in if they already exist. Always updates
 //facebook token
-
-  new User({facebookID: profile.id})
-    .fetch()
-    .then(function(model){
-      if(!model){
-        new User({
-          facebookID: profile.id,
-          name: profile.displayName,
-          facebookToken: accessToken
-        }, {isNew: true})
-        .save()
+      new User({facebookID: profile.id})
+        .fetch()
         .then(function(model){
-          return done(null, model);
-        });
-      } else {
-        console.log('user is found, resetting accessToken');
-        model.save({facebookToken: accessToken}, {patch: true})
-        .then(function(model){
-          return done(null, model);
-        });
-      }
-    })
-      // client.then(function(db) {
-      //   return db.collection('users').findOneAsync({ id: profile.id })
-      //   .then(function(user) {
-      //     if (!user) {
-      //       console.log('user NOT found, creating a new one...');
-      //       var newUser = {
-      //         id: profile.id,
-      //         name: profile.displayName,
-      //         FBtoken: accessToken
-      //       };
-      //       db.collection('users').insert(newUser);
-      //       return done(null, newUser);
-      //     }
-      //     else {
-      //       console.log('user is found, re-setting accessToken...');
-      //       db.collection('users').update(
-      //         {_id: user._id},
-      //         {$set:
-      //           {FBtoken : accessToken}
-      //         }
-      //       );
-      //       return done(null, user);
-      //     }
-      //   });
-      // });
-
+          if(!model){
+            new User({
+              facebookID: profile.id,
+              name: profile.displayName,
+              facebookToken: accessToken
+            }, {isNew: true})
+            .save()
+            .then(function(model){
+              return done(null, model);
+            });
+          } else {
+            console.log('user is found, resetting accessToken');
+            model.save({facebookToken: accessToken}, {patch: true})
+            .then(function(model){
+              return done(null, model);
+            });
+          }
+        })
   }));
 };
 
