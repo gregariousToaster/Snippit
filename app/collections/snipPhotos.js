2028a65775883055578e0a689d59dd869b0168dc
EOutterson <eoutterson@gmail.com>
2015-06-09 19:43:43 -0700

    cleared out example file

--- app/collections/snipPhotos.js
+++
@@ -1,8 +0,0 @@
-var db = require('../config');
-var SnipPhoto = require('../models/snipPhoto');
-
-var SnipPhotos = new db.Collection();
-
-SnipPhotos.model = SnipPhoto;
-
-module.exports = SnipPhotos;
